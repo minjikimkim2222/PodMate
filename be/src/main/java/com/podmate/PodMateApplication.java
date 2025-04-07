38af92384d20b74301a6becb28b376595aaeb474
minjikimkim2222 <olivia0929@naver.com>
2025-04-07 21:35:17 +0900

    프로젝트 기본구조 설정(be,fe 디렉터리), gitignore에 application.yml 추가

---
+++ be/src/main/java/com/podmate/PodMateApplication.java
@@ -0,0 +1,13 @@
+package com.podmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodMateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PodMateApplication.class, args);
+    }
+
+}
---
+++ be/src/test/java/com/podmate/PodMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.podmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodMateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
