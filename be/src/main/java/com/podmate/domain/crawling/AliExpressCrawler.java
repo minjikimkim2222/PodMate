f0c78a0c02555779d51822e909e079d0e1274339
minjikimkim2222 <olivia0929@naver.com>
2025-05-29 23:11:22 +0900

    알리 크롤러 배포 뚫어보기 시도1 - user-agent 설정 추가

--- be/src/main/java/com/podmate/domain/crawling/AliExpressCrawler.java
+++ be/src/main/java/com/podmate/domain/crawling/AliExpressCrawler.java
@@ -34,7 +34,7 @@ public class AliExpressCrawler implements PlatformCrawler{
         // 크롤링 봇 방지
         // 1. User-Agent 설정 (필수 - 봇 탐지 우회의 기본)
         // options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"); -- 윈도우 환경
-      //  options.addArguments("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36");  // -- mac 환경
+        options.addArguments("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36");  // -- mac 환경
 
         // 2. 자동화 제어 플래그 비활성화 (많은 사이트가 이 플래그로 봇 감지)
         options.addArguments("--disable-blink-features=AutomationControlled");
